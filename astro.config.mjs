51cc079f67d5ae5d53d076a06e56dd4c81ad776e
jairorodriguesgit <jairorodriguesapp@gmail.com>
2026-09-20 10:13:22 -0300

    Configurando Astro Base para o GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://jairorodriguesgit.github.io',
+  base: '/wavenoise-lab',
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
