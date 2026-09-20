8aa8950020da9db7753da9ba0c2a7945c37fa512
jairorodriguesgit <jairorodriguesapp@gmail.com>
2026-09-20 13:06:46 -0300

    Ajustes de URLs de Produtos e Paginas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://jairorodriguesgit.github.io',
   base: '/wavenoise-lab',
+  trailingSlash: 'ignore',
   vite: {
     plugins: [tailwindcss()],
   },
