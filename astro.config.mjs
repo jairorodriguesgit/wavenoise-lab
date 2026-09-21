a78695275021fba06d23916fa82f39878074ca3d
jairorodriguesgit <jairorodriguesapp@gmail.com>
2026-09-20 21:23:53 -0300

    Configurando Dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://jairorodriguesgit.github.io',
-  base: '/wavenoise-lab',
+  site: 'https://wavenoiselab.com.br',
+  base: '/',
   trailingSlash: 'ignore',
   vite: {
     plugins: [tailwindcss()],
